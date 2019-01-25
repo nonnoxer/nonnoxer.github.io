4c687659d7af15ff33f9dfcdc1bcdebae4806bc0
NATANAEL TAN TIONG OON <22YNATA978H@student.ri.edu.sg>
2019-01-25 14:52:56 +0800

    Update

--- source/script.js
+++ source/script.js
@@ -1,1 +1,3 @@
-function rickroll(){$("#rickroller").html('<p>hello world</p>');}
+function rickroll() {
+  $("#rickrolled").html('<iframe name="removethisisyoucan" width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>')
+}
