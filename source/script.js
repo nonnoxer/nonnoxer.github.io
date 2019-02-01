11f2d257136400a12d875959571c914751e0aaa2
NATANAEL TAN TIONG OON <22YNATA978H@student.ri.edu.sg>
2019-02-01 14:26:43 +0800

    Update

--- source/script.js
+++ source/script.js
@@ -1,3 +1,19 @@
 function goto() {
-  $("#gotoed").html('<iframe src="https://www.openprocessing.org/sketch/658208/embed/" width=100% height=100%></iframe><button><a href="https://nonnoxer.github.io">Go Back</a></button>')
+  $("#gotoed").html('<iframe src="https://www.openprocessing.org/sketch/658208/embed/" width=100% height=100%></iframe><button><a href="https://nonnoxer.github.io">Go Back</a></button>');
+}
+
+function count() {
+  alert("Let's play a game: Who can count the highest?");
+  var high = prompt("You start: how high can you count?");
+  alert(high + "? I can count MUCH higher than that");
+  alert("Prepare to be epic destroyed, human >:)");
+  for (var i = 1; i <= parseInt(high) + 1; i++) {
+    alert(i);
+  }
+  alert("#destroyed");
+}
+
+function destroy() {
+  $("body").addClass("animated hinge");
+  $("body").after('<a href="https://nonnoxer.github.io">Reverse time using space magic?</a>');
 }
