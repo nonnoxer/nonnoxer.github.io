622b4153d52988575ade92ccab8ae0dcf000cfa2
NATANAEL TAN TIONG OON <22YNATA978H@student.ri.edu.sg>
2020-02-13 08:44:19 +0800

    Game creation

--- source/script.js
+++ source/script.js
@@ -2,6 +2,21 @@ page_init = () => {
     $("#create").hide()
 }
 game_new = () => {
-    $("#start").hide();
-    $("#create").show();
+    $("#start").hide()
+    $("#create").show()
+    game_create_update()
+}
+game_create_update = () => {
+    attributes = ["itl", "hwk", "fit", "scb", "edg", "cpl"]
+    attributes.forEach(attr => {
+        $("#" + attr).text($("#" + attr + "_range").val())
+    });
+}
+game_create_finish = () => {
+    student_attr = {}
+    attributes = ["itl", "hwk", "fit", "scb", "edg", "cpl"]
+    attributes.forEach(attr => {
+        student_attr[attr] = $("#" + attr).text()
+    })
+    student_attr["nme"] = $("#nme").val()
 }
